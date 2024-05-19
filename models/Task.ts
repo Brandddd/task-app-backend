68ea1d79284355138f2b8b756f9778d57ecab734
Brandon Palacio <b.palacio1@utp.edu.co>
2024-05-18 22:38:27 -0500

    feat/taskAppBackend_v0.0.1: Model and Schema created for the tasks.

---
+++ models/Task.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  done: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// Export model based on the Schema created for the tasks
+export default model("Task", taskSchema);
