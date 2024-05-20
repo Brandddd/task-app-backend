12e226730a4aea28447d4adaf9302297b14e692b
Brandon Palacio <b.palacio1@utp.edu.co>
2024-05-20 00:19:58 -0500

    feat/taskApp_V0.2.0: CRUD completed.

--- routes/index.ts
+++ routes/index.ts
@@ -2,32 +2,55 @@ import { Router } from "express";
 import Task from "../models/Task";
 
 const router = Router();
-
 // All routes for the App
+// Get all Tasks
 router.get("/tasks", async (req, res) => {
   const tasks = await Task.find();
   res.send(tasks);
 });
+
+// Create a Task
 router.post("/tasks", async (req, res) => {
   // User sends a request with a title and description
   const { title, description } = req.body;
-
   // A new task is created with the desestrutured request data sent by user
   const task = new Task({ title, description });
-
   // Save data on the mongodb database
   await task.save();
-
   res.json(task);
 });
-router.get("/tasks/:id", (req, res) => {
-  res.send("Testing route Getting a unique Task!");
+
+// Get a Task
+router.get("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found!" });
+    res.send("Task deleted successfully");
+  } catch (error) {
+    res.send("A error has ocurred while trying to get task!");
+    console.error("A error has ocurred while trying to get task!");
+  }
 });
-router.delete("/tasks/:id", (req, res) => {
-  res.send("Testing route Deleting a Task!");
+
+// Delete a Task
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task)
+      return res.status(404).json({ message: "Task to delete not found!" });
+    res.send(task);
+  } catch (error) {
+    res.send("A error has ocurred while trying to delete task!");
+    console.error("A error has ocurred while trying to delete task!");
+  }
 });
-router.put("/tasks/:id", (req, res) => {
-  res.send("Testing route Updating a Task!");
+
+// Update a Task
+router.put("/tasks/:id", async (req, res) => {
+  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.send(updatedTask);
 });
 
 export default router;
